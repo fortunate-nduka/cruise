16bbcc7175153ba56d2cb09b7a5871a68d8452cc
Fortunate ogodu <58645821+iamfortunate@users.noreply.github.com>
2021-03-21 17:35:15 +0100

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    autoplay: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+    loop: true,
+});
